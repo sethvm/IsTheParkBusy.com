20f6a4dff0b87ae84c1fd7a6995c9ec323f6b2d3
Joshua Wilkinson <joshjeffreywilkinson@gmail.com>
2020-07-02 22:57:11 -0400

    initial commit

---
+++ backend/park_information.py
@@ -0,0 +1,6 @@
+import populartimes
+
+api_key = "AIzaSyARbfAurhEXFsuEp1SvJlq6OieCqPvpxWg"
+#Trinity Bellwood Park PlaceID
+place_id = "ChIJ29zuWfs0K4gRu3X7rsgi-wM"
+populartimes.get_id(api_key, place_id)
